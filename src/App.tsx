ed07e6a435f7abdf208001711ed6022cacf3b72e
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-23 17:18:26 -0500

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+            <li>
+              <Link to='/'><Home/></Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* <Switch> */}
+        <Route path='/about'><About/></Route>
+        <Route path='/contact'><Contact/></Route>
+        <Route path='/users'><Users/></Route>
+      {/* </Switch> */}
+    </Router>
   );
+}
+
+function Home() : React.ReactElement {
+  return (
+    <div>Home</div>
+  )
+}
+
+function About(): React.ReactElement {
+  return (
+    <div>About</div>
+  )
+}
+function Contact(): React.ReactElement {
+  return (
+    <div>Contact</div>
+  )
+}
+function Users(): React.ReactElement {
+  return (
+    <div>Users</div>
+  )
 }
 
 export default App;
